67fc2eadb39b3de9e2411e75f6f8c5a473782c08
Hvostet <113571051+Hvostet@users.noreply.github.com>
2022-12-14 12:23:15 +0300

    Add files via upload

---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+
+
+public class Cat {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setCat(String message) {
+        this.message = message;
+    }
+}
