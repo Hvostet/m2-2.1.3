30e4a0805092caeb7be4dc3daa11532b6343a45b
Hvostet <113571051+Hvostet@users.noreply.github.com>
2022-12-15 11:55:22 +0300

    Add files via upload

---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+import org.springframework.stereotype.Component;
+
+
+public class Cat {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setCat(String message) {
+        this.message = message;
+    }
+}
